1941ab5300d31bf02d720138f8b55f8f136196ba
Ainur08 <AjIGatin@stud.kpfu.ru>
2019-12-22 06:59:48 +0300

    interface added

---
+++ Semwork02/src/main/java/controller/MainController.java
@@ -0,0 +1,453 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MainController {
+    private int choise = 0;
+
+    @FXML
+    private Button btn1;
+    @FXML
+    private Button btn2;
+    @FXML
+    private Button btn3;
+    @FXML
+    private Button btn4;
+    @FXML
+    private Button btn5;
+    @FXML
+    private Button btn6;
+    @FXML
+    private Button btn7;
+    @FXML
+    private Button btn8;
+    @FXML
+    private Button btn9;
+    @FXML
+    private Button btn10;
+    @FXML
+    private Button btn11;
+    @FXML
+    private Button btn12;
+    @FXML
+    private Button btn13;
+    @FXML
+    private Button btn14;
+    @FXML
+    private Button btn15;
+    @FXML
+    private Button btn16;
+    @FXML
+    private Button btn17;
+    @FXML
+    private Button btn18;
+    @FXML
+    private Button btn19;
+    @FXML
+    private Button btn20;
+    @FXML
+    private Button btn21;
+    @FXML
+    private Button btn22;
+    @FXML
+    private Button btn23;
+    @FXML
+    private Button btn24;
+    @FXML
+    private Button btn25;
+    @FXML
+    private Button btn26;
+    @FXML
+    private Button btn27;
+    @FXML
+    private Button btn28;
+    @FXML
+    private Button btn29;
+    @FXML
+    private Button btn30;
+    @FXML
+    private Button btn31;
+    @FXML
+    private Button btn32;
+    @FXML
+    private Button btn33;
+    @FXML
+    private Button btn34;
+    @FXML
+    private Button btn35;
+    @FXML
+    private Button btn36;
+
+    @FXML
+    private void clickO(ActionEvent event) {
+        choise = 1;
+    }
+
+    @FXML
+    private void clickX(ActionEvent event) {
+        choise = 2;
+    }
+
+    @FXML
+    private void click1(ActionEvent event) {
+        if (choise == 1) {
+            btn1.setText("O");
+        } else if (choise == 2) {
+            btn1.setText("Х");
+        }
+        btn1.setDisable(true);
+    }
+
+    @FXML
+    private void click2(ActionEvent event) {
+        if (choise == 1) {
+            btn2.setText("O");
+        } else if (choise == 2) {
+            btn2.setText("Х");
+        }
+        btn2.setDisable(true);
+    }
+
+    @FXML
+    private void click3(ActionEvent event) {
+        if (choise == 1) {
+            btn3.setText("O");
+        } else if (choise == 2) {
+            btn3.setText("Х");
+        }
+        btn3.setDisable(true);
+    }
+
+    @FXML
+    private void click4(ActionEvent event) {
+        if (choise == 1) {
+            btn4.setText("O");
+        } else if (choise == 2) {
+            btn4.setText("Х");
+        }
+        btn4.setDisable(true);
+    }
+
+    @FXML
+    private void click5(ActionEvent event) {
+        if (choise == 1) {
+            btn5.setText("O");
+        } else if (choise == 2) {
+            btn5.setText("Х");
+        }
+        btn2.setDisable(true);
+    }
+
+    @FXML
+    private void click6(ActionEvent event) {
+        if (choise == 1) {
+            btn6.setText("O");
+        } else if (choise == 2) {
+            btn6.setText("Х");
+        }
+        btn2.setDisable(true);
+    }
+
+    @FXML
+    private void click7(ActionEvent event) {
+        if (choise == 1) {
+            btn7.setText("O");
+        } else if (choise == 2) {
+            btn7.setText("Х");
+        }
+        btn2.setDisable(true);
+    }
+
+    @FXML
+    private void click8(ActionEvent event) {
+        if (choise == 1) {
+            btn8.setText("O");
+        } else if (choise == 2) {
+            btn8.setText("Х");
+        }
+        btn2.setDisable(true);
+    }
+
+    @FXML
+    private void click9(ActionEvent event) {
+        if (choise == 1) {
+            btn9.setText("O");
+        } else if (choise == 2) {
+            btn9.setText("Х");
+        }
+        btn2.setDisable(true);
+    }
+
+    @FXML
+    private void click10(ActionEvent event) {
+        if (choise == 1) {
+            btn10.setText("O");
+        } else if (choise == 2) {
+            btn1.setText("Х");
+        }
+        btn2.setDisable(true);
+    }
+
+    @FXML
+    private void click11(ActionEvent event) {
+        if (choise == 1) {
+            btn11.setText("O");
+        } else if (choise == 2) {
+            btn11.setText("Х");
+        }
+        btn2.setDisable(true);
+    }
+
+    @FXML
+    private void click12(ActionEvent event) {
+        if (choise == 1) {
+            btn12.setText("O");
+        } else if (choise == 2) {
+            btn12.setText("Х");
+        }
+        btn2.setDisable(true);
+    }
+
+    @FXML
+    private void click13(ActionEvent event) {
+        if (choise == 1) {
+            btn13.setText("O");
+        } else if (choise == 2) {
+            btn13.setText("Х");
+        }
+        btn2.setDisable(true);
+    }
+
+    @FXML
+    private void click14(ActionEvent event) {
+        if (choise == 1) {
+            btn14.setText("O");
+        } else if (choise == 2) {
+            btn14.setText("Х");
+        }
+        btn2.setDisable(true);
+    }
+
+    @FXML
+    private void click15(ActionEvent event) {
+        if (choise == 1) {
+            btn15.setText("O");
+        } else if (choise == 2) {
+            btn15.setText("Х");
+        }
+        btn2.setDisable(true);
+    }
+
+    @FXML
+    private void click16(ActionEvent event) {
+        if (choise == 1) {
+            btn16.setText("O");
+        } else if (choise == 2) {
+            btn16.setText("Х");
+        }
+        btn2.setDisable(true);
+    }
+
+    @FXML
+    private void click17(ActionEvent event) {
+        if (choise == 1) {
+            btn17.setText("O");
+        } else if (choise == 2) {
+            btn17.setText("Х");
+        }
+        btn2.setDisable(true);
+    }
+
+    @FXML
+    private void click18(ActionEvent event) {
+        if (choise == 1) {
+            btn18.setText("O");
+        } else if (choise == 2) {
+            btn18.setText("Х");
+        }
+        btn2.setDisable(true);
+    }
+
+    @FXML
+    private void click19(ActionEvent event) {
+        if (choise == 1) {
+            btn19.setText("O");
+        } else if (choise == 2) {
+            btn19.setText("Х");
+        }
+        btn2.setDisable(true);
+    }
+
+    @FXML
+    private void click20(ActionEvent event) {
+        if (choise == 1) {
+            btn20.setText("O");
+        } else if (choise == 2) {
+            btn20.setText("Х");
+        }
+        btn2.setDisable(true);
+    }
+
+    @FXML
+    private void click21(ActionEvent event) {
+        if (choise == 1) {
+            btn21.setText("O");
+        } else if (choise == 2) {
+            btn21.setText("Х");
+        }
+        btn2.setDisable(true);
+    }
+
+    @FXML
+    private void click22(ActionEvent event) {
+        if (choise == 1) {
+            btn22.setText("O");
+        } else if (choise == 2) {
+            btn22.setText("Х");
+        } else btn1.setText("Error");
+        btn2.setDisable(true);
+    }
+
+    @FXML
+    private void click23(ActionEvent event) {
+        if (choise == 1) {
+            btn23.setText("O");
+        } else if (choise == 2) {
+            btn23.setText("Х");
+        }
+        btn2.setDisable(true);
+    }
+
+    @FXML
+    private void click24(ActionEvent event) {
+        if (choise == 1) {
+            btn24.setText("O");
+        } else if (choise == 2) {
+            btn24.setText("Х");
+        }
+        btn2.setDisable(true);
+    }
+
+    @FXML
+    private void click25(ActionEvent event) {
+        if (choise == 1) {
+            btn25.setText("O");
+        } else if (choise == 2) {
+            btn25.setText("Х");
+        }
+        btn2.setDisable(true);
+    }
+
+    @FXML
+    private void click26(ActionEvent event) {
+        if (choise == 1) {
+            btn26.setText("O");
+        } else if (choise == 2) {
+            btn26.setText("Х");
+        }
+        btn2.setDisable(true);
+    }
+
+    @FXML
+    private void click27(ActionEvent event) {
+        if (choise == 1) {
+            btn27.setText("O");
+        } else if (choise == 2) {
+            btn27.setText("Х");
+        }
+        btn2.setDisable(true);
+    }
+
+    @FXML
+    private void click28(ActionEvent event) {
+        if (choise == 1) {
+            btn28.setText("O");
+        } else if (choise == 2) {
+            btn28.setText("Х");
+        }
+        btn2.setDisable(true);
+    }
+
+    @FXML
+    private void click29(ActionEvent event) {
+        if (choise == 1) {
+            btn29.setText("O");
+        } else if (choise == 2) {
+            btn29.setText("Х");
+        }
+        btn2.setDisable(true);
+    }
+
+    @FXML
+    private void click30(ActionEvent event) {
+        if (choise == 1) {
+            btn30.setText("O");
+        } else if (choise == 2) {
+            btn30.setText("Х");
+        }
+        btn2.setDisable(true);
+    }
+
+    @FXML
+    private void click31(ActionEvent event) {
+        if (choise == 1) {
+            btn31.setText("O");
+        } else if (choise == 2) {
+            btn31.setText("Х");
+        }
+        btn2.setDisable(true);
+    }
+
+    @FXML
+    private void click32(ActionEvent event) {
+        if (choise == 1) {
+            btn32.setText("O");
+        } else if (choise == 2) {
+            btn32.setText("Х");
+        }
+        btn2.setDisable(true);
+    }
+
+    @FXML
+    private void click33(ActionEvent event) {
+        if (choise == 1) {
+            btn33.setText("O");
+        } else if (choise == 2) {
+            btn33.setText("Х");
+        }
+        btn2.setDisable(true);
+    }
+
+    @FXML
+    private void click34(ActionEvent event) {
+        if (choise == 1) {
+            btn34.setText("O");
+        } else if (choise == 2) {
+            btn34.setText("Х");
+        }
+        btn2.setDisable(true);
+    }
+
+    @FXML
+    private void click35(ActionEvent event) {
+        if (choise == 1) {
+            btn35.setText("O");
+        } else if (choise == 2) {
+            btn35.setText("Х");
+        }
+        btn2.setDisable(true);
+    }
+
+    @FXML
+    private void click36(ActionEvent event) {
+        if (choise == 1) {
+            btn36.setText("O");
+        } else if (choise == 2) {
+            btn36.setText("Х");
+        }
+        btn2.setDisable(true);
+    }
+
+}
